d2c5598669deb1de4c7a2a81ff2b4a9e76cba867
Glitch (steven-quartz-universe) <none>
2018-03-12 14:49:16 +0000

    👄🎈 Updated with Glitch

--- index.js
+++ index.js
@@ -16,8 +16,13 @@ const client = new Discord.Client();
 client.on('ready',() => {
   console.log('I\'m Online\nI\'m Online');
 });
+
+var streamingGame = {type: 1, name: "Steven Universe: Save the Light", url: "https://twitch.tv/jtrent238"}; // "Streaming"
+// note: streaming status requires a valid twitch url:
+//       ex. "http://twitch.tv/channel"
 var quotes = ["quote1", "quote2", "quote3"]
 var episodes = ["S1E1 Gem Glow", "S1E2 Laser Light Cannon", "S1E3 Cheeseburger Backpack", "S1E4 Together Breakfast", "S1E5 Frybo", "S1E6 Cat Fingers", "S1E7 Bubble Buddies", "S1E8 Serious Steven", "S1E9 Tiger Millionaire", "S1E10 Steven's Lion", "S1E11 Arcade Mania", "S1E12 Giant Woman", "S1E13 So Many Birthdays", "S1E14 Lars and the Cool Kids", "S1E15 Onion Trade"] 
+
 
 const prefix = '~';
 client.on('message', message => {
@@ -35,6 +40,13 @@ client.on('message', message => {
     message.channel.sendMessage('<:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785>');
     
   }
+
+  //Information about the bot.
+  if (message.content.startsWith(prefix + 'info')) {
+    message.channel.sendMessage('<:NewRoseGem:422744912182902785> Bot Version:' + process.env.VERSION);
+    message.channel.sendMessage('<:NewRoseGem:422744912182902785> Bot made by: ' + process.env.AUTHOR);
+  }
+  
   
   if (message.content.startsWith(prefix + 'ping')) {
     message.channel.sendMessage('pong');
@@ -53,6 +65,11 @@ client.on('message', message => {
   //Shows Steven's Gem
   if (message.content.startsWith(prefix + 'stevengem')) {
     message.channel.sendMessage('My Gem is: <:NewRoseGem:422744912182902785>, a Rose Quartz.');
+  }
+  
+  //Steven Loves Cookie Cat
+  if (message.content.startsWith('Cookie Cat')) {
+    message.channel.sendMessage('I Love Cookie Cat!');
   }
   
 });
