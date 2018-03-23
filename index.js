634371df016cdd3e77b873d05e82eb870c038c02
Glitch (steven-quartz-universe) <none>
2018-03-23 16:36:09 +0000

    👷🍉 Updated with Glitch

--- index.js
+++ index.js
@@ -16,7 +16,23 @@ Raven.config(process.env.DSN_PUBLIC, {
   release: process.env.SENTRY_VERSION
 });
 
+/**
+ * Report a routing error to Sentry and show a feedback dialog to
+ * the user.
+ * 
+ * > try {
+ * >   renderRoute()
+ * > } catch (err) {
+ * >   handleRouteError(err);
+ * > }
+ */
+function handleRouteError(err) {
+  Raven.captureException(err);
+  Raven.showReportDialog();
+};
+
 var SpotifyWebApi = require('spotify-web-api-js');
+var spotify = require('spotify');
 var spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(process.env.SPOTIFYAPI);
 
@@ -32,6 +48,7 @@ const prefix = '~';
 client.on('ready',() => {
   console.log('Hi, It\'s Steven!');
   console.log(`Logged in as ${client.user.tag}!`);
+  //game(streamingGame);
 });
 
 
@@ -107,6 +124,8 @@ client.on('message', message => {
     //message.channel.sendMessage('<:NewRoseGem:422744912182902785> patreon = It's My Patreon'); 
     //message.channel.sendMessage('<:NewRoseGem:422744912182902785> echo = I repeat what ever you say. Echoooo!!!'); 
     //message.channel.sendMessage('<:NewRoseGem:422744912182902785> nsfw = You know what this is! Right?'); 
+    //message.channel.sendMessage('<:NewRoseGem:422744912182902785> srcarchivedl = Sends an archive of the src code.'); 
+  
     //message.channel.sendMessage('<:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785>');
     
   //}
@@ -115,7 +134,7 @@ client.on('message', message => {
   if (message.content.startsWith(prefix + 'help')) {
     //message.channel.sendMessage('this is a help command for the bot.');
     message.channel.sendMessage('<:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785>\n<:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> The prefix for the bot is `' + prefix + '` <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785>\n<:NewRoseGem:422744912182902785> info = Information about the bot. :exclamation: \n<:NewRoseGem:422744912182902785> help = Shows this message. :question: \n<:NewRoseGem:422744912182902785> ping = pong\n<:NewRoseGem:422744912182902785> uptime = Gets the bots uptime.\n<:NewRoseGem:422744912182902785> stevenquote = Random quote from the show.\n<:NewRoseGem:422744912182902785> stevenepisode = Random episode.\n<:NewRoseGem:422744912182902785> stevengem = Shows what gem Steven is. <:NewRoseGem:422744912182902785>\n<:NewRoseGem:422744912182902785> invitesteven = Generates a link to invite Steven to a server.\n<:NewRoseGem:422744912182902785> github = Gives the bots Github link. <:github:401011847538671627>\n<:NewRoseGem:422744912182902785> cookiecat = I Love Cookie Cat! <:cookiecat:423144575650103317>\n<:NewRoseGem:422744912182902785> logo = Steven Universe Logo!.');
-    message.channel.sendMessage('<:NewRoseGem:422744912182902785> roll = I will roll a dice numberd 1-6. :game_die: \n<:NewRoseGem:422744912182902785> servercount = How many servers am I in?\n<:NewRoseGem:422744912182902785> coin = I will flip a coin! :coin: \n<:NewRoseGem:422744912182902785> today = Todays Date!\n<:NewRoseGem:422744912182902785> patreon = It\'\s My Patreon! <:patreon_logo:388799943437058059>\n<:NewRoseGem:422744912182902785> echo = I repeat what ever you say. Echoooo!!!\n<:NewRoseGem:422744912182902785> nsfw = You know what this is! Right?\n<:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785>');
+    message.channel.sendMessage('<:NewRoseGem:422744912182902785> roll = I will roll a dice numberd 1-6. :game_die: \n<:NewRoseGem:422744912182902785> servercount = How many servers am I in?\n<:NewRoseGem:422744912182902785> coin = I will flip a coin! :coin: \n<:NewRoseGem:422744912182902785> today = Todays Date!\n<:NewRoseGem:422744912182902785> patreon = It\'\s My Patreon! <:patreon_logo:388799943437058059>\n<:NewRoseGem:422744912182902785> echo = I repeat what ever you say. Echoooo!!!\n<:NewRoseGem:422744912182902785> nsfw = You know what this is! Right?\n <:NewRoseGem:422744912182902785> srcarchivedl = Sends an archive of the src code.' + '\n<:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785> <:NewRoseGem:422744912182902785>');
     
   }
 
@@ -278,6 +297,16 @@ client.on('message', message => {
       }, function(err) {
         console.error(err);
       });    
+    
+    spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    // Do something with 'data' 
+      message.channel.sendMessage('Here is what I found: ' + data);
+    });
   }
   
   //Gets a player's Minecraft skin.
