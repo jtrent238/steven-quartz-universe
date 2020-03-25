08540de74a490d7cd55b4b69be6ec4703e9d54e6
Glitch (steven-quartz-universe) <none>
2020-03-25 01:09:22 +0000

    🐓🚶 Updated with Glitch

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+//const Discord = require('discord.js');
+//const client = new Discord.Client();
+
 const http = require('http');
 const express = require('express');
 const app = express();
+
+//const settings = require('./settings.json');
+
 app.get("/", (request, response) => {
-    console.log(Date.now() + " Ping Received");
-    response.sendStatus(200);
-    //response.sendFile(__dirname + '/site/index.html');
-
-    app.use(morgan('combined'))
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+  //response.sendFile(__dirname + '/site/index.html');
+  
+  //app.use(morgan('combined'))
 });
-
-app.get('/', function (req, res) {
-    res.send('hello, world!')
-})
 
 app.listen(process.env.PORT);
 setInterval(() => {
